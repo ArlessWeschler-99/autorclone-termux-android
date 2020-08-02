6bf77cc41491d80659de8d873641b3153abcb21e
Arless Weschler <67251343+ArlessWeschler-99@users.noreply.github.com>
2020-08-02 01:57:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 cd /sdcard
 
-apt update && apt upgrade && apt install git  && apt install python -y
+apt update && apt upgrade && apt install git -y
 
-git clone  https://github.com/xyou365/AutoRclone && cd AutoRclone && pip install -r requirements.txt
+git clone  https://github.com/xyou365/AutoRclone
 
-apt install rclone -y
+apt install python && apt install rclone -y
 
-cd /sdcard/AutoRclone/
-
-rm -r .git
+cd /sdcard/AutoRclone && pip install -r requirements.txt && rm -r .git
 
 echo "Autorclone satisfactoriamente instalado. Continúa con los siguientes pasos; https://telegra.ph/Autorclone-en-Android-Termux-07-28"
