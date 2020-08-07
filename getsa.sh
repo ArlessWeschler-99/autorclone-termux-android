936b4176bb38cd1c387ce3d70f739bc2a8851d0c
Arless Weschler <67251343+ArlessWeschler-99@users.noreply.github.com>
2020-08-07 16:17:19 -0500

    Update getsa.sh

--- getsa.sh
+++ getsa.sh
@@ -8,4 +8,4 @@ cd $HOME
 
 rm -rf getsa.sh
 
-echo "Correos extraídos satisfactoriamente. Verá sus correos en un archivo llamado "email.txt". Ábralo y edite todos los saltos de línea con una coma (,). Recuerde que sólo se permite añadir 10 correos al mismo tiempo y separados por una coma"
+echo "Correos extraídos satisfactoriamente. Verá sus correos en un archivo llamado email.txt. Ábralo y edite todos los saltos de línea con una coma (,). Recuerde que sólo se permite añadir 10 correos al mismo tiempo y separados por una coma"
